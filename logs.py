cb7437f85d61dd328123f4083a4dd7db1b6e9f65
Pradeepbabu Ramia Jeevan <rjdeepu@gmail.com>
2018-04-02 08:10:27 -0700

    Modified code to display proper output

--- logs.py
+++ logs.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import psycopg2
 
 # Connecting to DB
@@ -6,29 +7,35 @@ db = psycopg2.connect("dbname=news")
 cursor = db.cursor()
 
 # Query to find Most Popular three articles of all time
-cursor.execute("select title, count(1) as cnt from log join articles as art "
-               "on substr(log.path,10) = art.slug "
-               "group by title order by cnt desc limit 3")
-result1 = cursor.fetchall()
-print (result1)
+cursor.execute("SELECT title, count(1) as cnt FROM log JOIN articles AS art "
+               "ON SUBSTR(log.path,10) = art.slug "
+               "GROUP BY title ORDER BY cnt DESC LIMIT 3")
+print ('')
+print ("What are the most popular three articles of all time?")
+for i in cursor.fetchall():
+    print (i[0] + ' - ' + str(i[1]))
 
 # Query to find Most popular article authors of all time
-cursor.execute("select auth.name, count(1) as views from log join articles "
-               "as art on substr(log.path,10) = art.slug join authors auth "
-               "on art.author = auth.id "
-               "group by auth.name order by views desc")
-result2 = cursor.fetchall()
-print (result2)
+cursor.execute("SELECT auth.name, count(1) AS views FROM log JOIN articles "
+               "AS art ON SUBSTR(log.path,10) = art.slug JOIN authors auth "
+               "ON art.author = auth.id "
+               "GROUP BY auth.name ORDER BY views desc")
+print ('')
+print ("Who are the most popular article authors of all time")
+for i in cursor.fetchall():
+    print (i[0] + ' - ' + str(i[1]))
 
 # Query to find on which days more than 1% of requests leads to errors
-cursor.execute("select to_char(totdt, 'MONTH DD,YYYY'), "
-               "round(cast (errcnt as decimal) * 100 /totcnt,2) as perc "
-               "from (select time::date as totdt, count(1) as totcnt "
-               "from log group by time::date) as tot, (select time::date as "
-               "errdt, count(1) as errcnt from log where substr(status,1,3) "
-               "= '404' group by time::date) as err "
-               "where totdt = errdt and errcnt * 100 /totcnt > 1")
-result3 = cursor.fetchall()
-print (result3)
+cursor.execute("SELECT TO_CHAR(totdt, 'MONTH DD,YYYY'), "
+               "ROUND(CAST (errcnt AS decimal) * 100 /totcnt,2) AS perc "
+               "FROM (SELECT time::date AS totdt, count(1) AS totcnt "
+               "FROM log GROUP BY time::date) AS tot, (SELECT time::date AS "
+               "errdt, count(1) AS errcnt FROM log WHERE SUBSTR(status,1,3) "
+               "= '404' GROUP BY time::date) AS err "
+               "WHERE totdt = errdt AND errcnt * 100 /totcnt > 1")
+print ('')
+print ("On which days did more than 1% of requests lead to errors?")
+for i in cursor.fetchall():
+    print (i[0] + ' - ' + str(i[1]))
 
 db.close()
